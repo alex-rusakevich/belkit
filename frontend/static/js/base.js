c5e80b680841f674ead7993006461cc3d3ecac3e
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2024-09-21 10:10:09 +0300

    #patch website theme changes dynamically with system's

--- frontend/static/js/base.js
+++ frontend/static/js/base.js
@@ -1,5 +1,12 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-} else {
-    document.documentElement.setAttribute("data-bs-theme", "light");
+function changeWebsiteThemeAccrodingToSystem(){
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute("data-bs-theme", "dark");
+    } else {
+        document.documentElement.setAttribute("data-bs-theme", "light");
+    }
 }
+
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    changeWebsiteThemeAccrodingToSystem();
+});
