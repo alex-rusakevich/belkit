f4bf0b985d8c5723350e902ba08eeb841b04e153
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-01-01 16:04:47 +0300

    #patch install nextjs

--- belkit/asgi.py
+++ belkit/asgi.py
@@ -1,16 +1,31 @@
-"""
-ASGI config for belkit project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
+from django.urls import path, re_path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'belkit.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "belkit.settings")
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf import settings
+from django_nextjs.proxy import NextJSProxyHttpConsumer, NextJSProxyWebsocketConsumer
+
+http_routes = [re_path(r"", django_asgi_app)]
+websocket_routers = []
+
+if settings.DEBUG:
+    http_routes.insert(
+        0, re_path(r"^(?:_next|__next|next).*", NextJSProxyHttpConsumer.as_asgi())
+    )
+    websocket_routers.insert(
+        0, path("_next/webpack-hmr", NextJSProxyWebsocketConsumer.as_asgi())
+    )
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": URLRouter(http_routes),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_routers)),
+    }
+)
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django_nextjs.render import render_nextjs_page
 
-# Create your views here.
+
+async def next_js_page(request):
+    # Your custom logic
+    return await render_nextjs_page(request)
