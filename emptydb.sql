9d7724c9d04560f4aa8a9e0929c689d9f7f05f88
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-01-01 15:04:40 +0300

    #major project reset: new one will be on nextjs

---
+++ emptydb.sql
@@ -0,0 +1,9 @@
+CREATE USER 'belkit_db_user'@'localhost';
+
+CREATE DATABASE belkit_db;
+ALTER DATABASE belkit_db CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+alter user 'belkit_db_user'@'localhost' identified by '__belkit';
+
+GRANT ALL PRIVILEGES ON belkit_db.* To 'belkit_db_user'@'localhost';
+flush privileges;
--- frontend/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
-
-# Register your models here.
-admin.site.site_title = _("BelKit editor & admin panel")
-admin.site.site_header = _("BelKit editor & admin panel")
