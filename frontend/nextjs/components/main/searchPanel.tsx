a42b64a8030737f05d7d12dc2be0c4afe558f8c0
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-01-02 20:59:53 +0300

    #patch moved menu to center

--- frontend/nextjs/components/main/searchPanel.tsx
+++ frontend/nextjs/components/main/searchPanel.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { getDictionary } from "@/lang/dictionary";
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
 import Link from "next/link"
 import { ChartColumnIncreasing, CirclePlus, House, List, Info } from "lucide-react";
@@ -9,6 +9,45 @@ import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { ScrollBar } from "@/components/ui/scroll-area";
 import React from "react";
 import { cn } from "@/lib/utils"
+
+
+const components: { title: string; href: string; description: string }[] = [
+    {
+        title: "Alert Dialog",
+        href: "/docs/primitives/alert-dialog",
+        description:
+            "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+        title: "Hover Card",
+        href: "/docs/primitives/hover-card",
+        description:
+            "For sighted users to preview content available behind a link.",
+    },
+    {
+        title: "Progress",
+        href: "/docs/primitives/progress",
+        description:
+            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+    },
+    {
+        title: "Scroll-area",
+        href: "/docs/primitives/scroll-area",
+        description: "Visually or semantically separates content.",
+    },
+    {
+        title: "Tabs",
+        href: "/docs/primitives/tabs",
+        description:
+            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+    },
+    {
+        title: "Tooltip",
+        href: "/docs/primitives/tooltip",
+        description:
+            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+    },
+]
 
 
 const ListItem = React.forwardRef<
@@ -45,7 +84,7 @@ const SearchPanelMenu = () => {
         rounded-xl border shadow w-full 
         max-w-2xl justify-start p-[4px]
         first:[&_svg]:pr-2">
-        <ScrollArea className="overflow-x-auto">
+        <ScrollArea className="overflow-x-auto mx-auto">
             <NavigationMenuList className="justify-start mx-auto">
                 <NavigationMenuItem>
                     <Link href="#" legacyBehavior passHref>
@@ -75,12 +114,23 @@ const SearchPanelMenu = () => {
                 </NavigationMenuItem>
 
                 <NavigationMenuItem>
-                    <Link href="#" legacyBehavior passHref>
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            <List />
-                            <span>{dictionary.additionalUtils}</span>
-                        </NavigationMenuLink>
-                    </Link>
+                    <NavigationMenuTrigger>
+                        <List />
+                        <span>{dictionary.additionalUtils}</span>
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] h-[200px] overflow-y-auto">
+                            {components.map((component) => (
+                                <ListItem
+                                    key={component.title}
+                                    title={component.title}
+                                    href={component.href}
+                                >
+                                    {component.description}
+                                </ListItem>
+                            ))}
+                        </ul>
+                    </NavigationMenuContent>
                 </NavigationMenuItem>
 
                 <NavigationMenuItem>
